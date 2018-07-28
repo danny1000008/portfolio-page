aa0bd19433454ce0274c01c152fc7ca971e4507f
Danny Wagstaff <danny1000008@yahoo.com>
2018-07-28 09:04:35 -0500

    added to portfolio section
    Added roman numeral converter app to portfolio section

--- form_to_email.php
+++ form_to_email.php
@@ -9,7 +9,7 @@ function IsInjected($str)
            '(%08+)',
            '(%09+)'
            );
-             
+
     $inject = join('|', $injections);
     $inject = "/$inject/i";
     if(preg_match($inject,$str))
@@ -36,25 +36,6 @@ $vis_name = $_POST['visName'];
 $vis_email = $_POST['visEmail'];
 $vis_phone = $_POST['visPhone'];
 $vis_msg = $_POST['visMsg'];
-
-// validate form data
-/* commented out because I decided to use client-side HTMLand JS instead
-if(empty($vis_name))
-{
-    echo 'Please enter your name.';
-    exit;
-}
-if(empty($vis_email) && empty($vis_phone))
-{
-    echo 'Please enter your email address or phone number so I can get back to you.';
-    exit;
-}
-if(empty($vis_msg))
-{
-    echo 'Please enter a message. This is a message form, after all!';
-    exit;
-}
-*/
 
 // email creation
 $email_to = 'danny1000008@yahoo.com';
